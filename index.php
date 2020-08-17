8a4c3b688a2cd24e3b2500d38a1750b7cff46fe8
Sergio <sergioralandison@outlook.com>
2020-08-17 16:26:57 +0200

    code update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!--Formulaire checkbox pour afficher les fichiers masqués-->
-<form class="" action="index.php" method="GET">
+<form class="" action="" method="GET">
   <label for="">Afficher les fichiers masqués</label>
   <input type="checkbox" name="hidden" value="checked">
   <button type="submit" name="button" value="send">Envoyer</button>
